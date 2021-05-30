b11ad8105936cfcd1cf4cd85b6ae89b44f452f32
Deon Zoss <deonquentinzoss@gmail.com>
2021-05-30 14:04:59 -0500

    Finish up proper display of keys

--- Display_Chord.py
+++ Display_Chord.py
@@ -4,7 +4,7 @@
 # Date: 20210529
 # Purpose: Ascii-based display showing how to play a provided chord
 #
-# Example: The following output shows how to play a D-chord
+# Example: The following output shows how to play a D-major
 #
 #      |    ###   ###    |    ###   ###   ###    |
 #      |    ###   ###    |    ###   ###   ###    |
@@ -18,7 +18,7 @@
 #      |_____|_____|_____|_____|_____|_____|_____|
 #  D:           X              X        X 
 #
-# Example: The same D-chord, with an alternative display
+# Example: The same D-major chord, with an alternative display
 #
 #      |    | |###| |    |    |#|   | |###| |    |
 #      |    | |###| |    |    |#|   | |###| |    |
@@ -30,7 +30,7 @@
 #      |     |#####|     |     |     |#####|     |
 #      |  C  |# D #|  E  |  F  |  G  |# A #|  B  |
 #      |_____|#####|_____|_____|_____|#####|_____|
-#                          D    
+#                           D    
 #
 ###############################################################################
 import argparse;
@@ -53,36 +53,107 @@ class Keys(Enum):
     B  = 12;
 
 ###############################################################################
-def buildKeys(startKey,numKeys):
+def buildKeys(startKey,numTotalKeys):
+    
+    currentKey=startKey.value;
+    currentNumDisplayed=0;
+    display=[];
 
-    WHITE_KEY=["|    ",\
-               "|    ",\
-               "|    ",\
-               "|    ",\
-               "|    ",\
-               "|    ",\
-               "|    ",\
-               "|  X ",\
-               "|____"];
+    while(currentNumDisplayed < numTotalKeys):
 
-    BLACK_KEY=["###",\
-               "###",\
-               "###",\
-               "###",\
-               "###",\
-               " | ",\
-               " | ",\
-               " | ",\
-               "_|_"];
+        display = addKeytoDisplay(currentKey,display);
+
+        if currentKey==len(Keys):
+            currentKey=1;
+        else:
+            currentKey+=1;
+
+        currentNumDisplayed+=1;
+
+    for row in display:
+        print(row);
+    
+def addKeytoDisplay(currentKey,display):    
+
+    WHITE_KEY_VALUES=[1,3,5,6,8,10,12];
+
+    WHITE_KEY=["   ",\
+               "   ",\
+               "   ",\
+               "   ",\
+               "   ",\
+               "   ",\
+               "   ",\
+               " X ",\
+               "___"];
+
+    WHITE_SEPARATOR=[" | ",\
+                     " | ",\
+                     " | ",\
+                     " | ",\
+                     " | ",\
+                     " | ",\
+                     " | ",\
+                     " | ",\
+                     "_|_"];
+
+    BLACK_KEY=["##",\
+               "##",\
+               "##",\
+               "##",\
+               "##",\
+               "| ",\
+               "| ",\
+               "| ",\
+               "|_"];
+
+    BLACK_SEPARATOR=["#",\
+                     "#",\
+                     "#",\
+                     "#",\
+                     "#",\
+                     " ",\
+                     " ",\
+                     " ",\
+                     "_"];
+
+    if currentKey in WHITE_KEY_VALUES:
+        COLOR = "WHITE";
+    else:
+        COLOR = "BLACK";
+
+    if display == []:
+        if COLOR == "WHITE":
+            display = WHITE_SEPARATOR.copy();
+        else:
+            display = BLACK_SEPARATOR.copy();
+    
+    if COLOR == "WHITE":
+        for row in range(len(WHITE_KEY)):
+            display[row]+=WHITE_KEY[row];
+        if ((currentKey+1)%len(Keys)) in WHITE_KEY_VALUES:
+            for row in range(len(WHITE_KEY)):
+                display[row]+=WHITE_SEPARATOR[row];
+        else:
+            for row in range(len(BLACK_KEY)):
+                display[row]+=BLACK_SEPARATOR[row];
+    else:
+        for row in range(len(BLACK_KEY)):
+            display[row]+=BLACK_KEY[row];
+
+        
+
+    return display;
 
 ###############################################################################
 def getArgs():
     parser = argparse.ArgumentParser( \
              description='Ascii-based display showing'\
                          ' how to play a provided chord');
-    parser.add_argument('-S','--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
+    parser.add_argument('-S','--start-key', dest='startKey', default='C',
+                        help='The musical note to start the display on,'\
+                             ' choose from {C,Db,D,Eb,E,F,Gb,G,Ab,A,Bb,B}'\
+                             ' (Default is C)');
     args = parser.parse_args();
     return args;
 
@@ -93,7 +164,7 @@ def main():
     #for index in range(len(WHITE_KEY)):
     #    print(WHITE_KEY[index]+BLACK_KEY[index]);
     
-    print(repr(Keys.D));
+    buildKeys(Keys[args.startKey],15);
 
 
 if __name__ == "__main__":
